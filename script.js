904b00874191d096d286cd4d526cac043f054ba5
Gingercopia <wtkline16@gmail.com>
2021-09-24 13:28:47 -0400

    Simple Interest Calculator:Updated with js and css for greater interactivity and style

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+//Value next to slider in range is updated
+function updateRate()
+    {
+        var rateval = document.getElementById("rate").value;
+        document.getElementById("rate_val").innerText=rateval;
+
+    }
+
+
+//Output displayed based on user input
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%.</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\><br\>"
 }
+
+
+
+//validate Principal input box not zero or negative values and if so show alert
+function pvalidation()
+    {
+        var principal = document.getElementById("principal").value;
+        
+        if (principal<=0 || principal == NaN){
+            alert("Enter a positive number!");
+            document.getElementById("principal").focus();
+        }
+        else {
+            compute();
+        }
+
+    }
+
+
+
+
         
